563031414f18346946f8fd49ae39bd8402ca6a4f
AhmedLabibb <92722943+AhmedLabibb@users.noreply.github.com>
2022-08-17 10:08:53 +0400

    Add Missing annotation

--- src/main/java/com/tamara/tamarapaymentapi/api/entity/repositories/OrderRepository.java
+++ src/main/java/com/tamara/tamarapaymentapi/api/entity/repositories/OrderRepository.java
@@ -2,6 +2,8 @@ package com.tamara.tamarapaymentapi.api.entity.repositories;
 
 import com.tamara.tamarapaymentapi.api.entity.Order;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends CrudRepository<Order,Long> {
 }
